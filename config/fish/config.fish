edb6a54519e49e94873846b9fbe9798d5dfaff75
Ivo Kosir <ivo.kosir93@gmail.com>
2017-02-11 22:36:19 +0100

    Add color to i3lock

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,8 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 
+set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
+
 set -x PATH $HOME/.local/bin $PATH
 set -x VISUAL /usr/bin/nvim
 set -x EDITOR $VISUAL
