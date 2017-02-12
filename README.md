3814b387a62483cbdecc54881b1065b97f3cd491
Ivo Kosir <ivo.kosir93@gmail.com>
2017-02-12 22:00:11 +0100

    Added font config

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # dotfiles
 My custom configs.
 
-Installer creates symlinks to configuration files, that way
-any change will happen in both files, so changes can be commited.
-
-When creating symlink:
-  * if file exists
-    * and it is a symlink -> just overwrite
-    * else rename /file/ to /file/.BACKUP
-
-Creates folders for custom files like bash and less history.
+Installer just deletes file in destination without any mercy and
+makes a copy from dotfiles.
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_DATA_HOME $HOME/.local/share
 
-set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
+#set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
 
 set -x PATH $HOME/.local/bin $PATH
 set -x VISUAL /usr/bin/nvim
@@ -10,3 +10,5 @@ set -x EDITOR $VISUAL
 set -x TERMINAL /usr/bin/gnome-terminal
 set -x PAGER /usr/bin/less
 set -x TERM xterm-256color
+
+set -x GTK2_RC_FILES /usr/share/themes/Arc/gtk-2.0/gtkrc
